170746d29f6d2ce747c5661db6a3fc10b3cc7ba1
gaurav0330 <128597810+gaurav0330@users.noreply.github.com>
2025-01-23 18:07:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -57,7 +57,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
